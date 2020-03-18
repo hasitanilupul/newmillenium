d433004cbcbdd9c9ff6955d6c3fdde023c676182
hasitanilupul <48640389+hasitanilupul@users.noreply.github.com>
2020-03-19 01:55:54 +0530

    work

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDbECRJPlAlGH_U8OOg2cBZSUdchfFXtgE",
+    authDomain: "millenium-f4bba.firebaseapp.com",
+    databaseURL: "https://millenium-f4bba.firebaseio.com",
+    projectId: "millenium-f4bba",
+    storageBucket: "millenium-f4bba.appspot.com",
+    messagingSenderId: "129735374754",
+    appId: "1:129735374754:web:58f84eb22c4c93639564fb"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDbECRJPlAlGH_U8OOg2cBZSUdchfFXtgE",
+    authDomain: "millenium-f4bba.firebaseapp.com",
+    databaseURL: "https://millenium-f4bba.firebaseio.com",
+    projectId: "millenium-f4bba",
+    storageBucket: "millenium-f4bba.appspot.com",
+    messagingSenderId: "129735374754",
+    appId: "1:129735374754:web:58f84eb22c4c93639564fb"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
